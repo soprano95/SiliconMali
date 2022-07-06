a18568cf3d89742be6d6a35fd5ced8fa74aba7eb
soprano95 <sacko202@gmail.com>
2022-07-06 13:58:46 +0000

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
   
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  root to: 'pages#home'
+  get 'about_silicon-mali',  to: 'pages#about'
+  get 'silicon-mali_contact', to: 'pages#contact'
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
