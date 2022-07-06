9f24ed587739d71acb16ac25e8fd47dbdb9e890d
soprano95 <sacko202@gmail.com>
2022-07-06 16:48:47 +0000

    implemented custom action for update statuses for blog post

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   get 'about_silicon-mali',  to: 'pages#about'
   get 'silicon-mali_contact', to: 'pages#contact'
   resources :blogs
+
+   resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
