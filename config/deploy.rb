fef0a83c1d5d123e44ce408417aea51030dca5a7
soprano95 <sacko202@gmail.com>
2022-07-22 18:18:06 +0000

    qowierjd

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, "main"
 
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/siliconmalis/#{fetch :application}"
+set :deploy_to, "/home/siliconmali/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
